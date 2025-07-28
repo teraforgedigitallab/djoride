502a4577026309fce5aadeb4d451b35917430260
teraforgedigitallab <teraforgedigitallab@gmail.com>
2025-07-28 23:19:37 +0530

    Update CitySelector.jsx

--- src/components/CitySelector.jsx
+++ src/components/CitySelector.jsx
@@ -117,7 +117,7 @@ const CitySelector = ({ onCitySelect }) => {
       opacity: 1, 
       y: 0,
       transition: { 
-        duration: 0.2,
+        duration: 0.3,
         ease: "easeOut"
       }
     }
